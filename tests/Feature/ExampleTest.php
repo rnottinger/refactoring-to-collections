529bda16308be1ace86a8eabc1d497d023019d30
Richard Ottinger <richard.ottinger@gmail.com>
2021-10-04 16:59:30 -0500

    Initial Commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+        //Test code
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+        //Test code
+    }
+}
